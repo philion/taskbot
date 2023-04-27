69a5bf8df869625dcd41b670ecd71d63d9bbb413
Paul Philion <pphlilion@istreamplanet.com>
2023-04-26 22:41:39 -0700

    got simple github call working. next step: add simple commands

--- README.md
+++ README.md
@@ -9,9 +9,19 @@ A Discord taskbot that uses GitHub tasks as a backing store.
     GITHUB_TOKEN=your-github-token
 
 
+## v0.1
+The prototype is working, now to add the bones: simple commands for adding, editing and checking on open tickets. First step is simple use cases.
+
+* list open issues, scoped by forum, with param for project, user, etc. list #project, list @user
+* status - status report of # open issues and 
+* add -
+* update - 
+
 ## Initial Prototype - working notes
 
 * Get initial bot working using [discord.py](https://discordpy.readthedocs.io/). DONE
 * Adding token management with `.env` file. DONE
 * Adding simple github API call to list issues.
 * Failure with initial attempt with ghapi. Terrible docs, no example code, cryptic errors. Blah.
+* Simple implementation using https://github.com/PyGithub/PyGithub has got list issues working. YAY!
+* Demonstrated: Access to both discord and github to access relevant data end-to-end.
--- taskbot.py
+++ taskbot.py
@@ -3,15 +3,21 @@
 
 import discord
 import os
-import sys
 
 from dotenv import load_dotenv
-from ghapi.all import GhApi
+from github import Github
+
 
 # setup client
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+# load the secrets
+load_dotenv()
+discord_token = os.getenv("DISCORD_TOKEN")
+github_token = os.getenv("GITHUB_TOKEN")
+gh = Github(github_token)
 
 
 @client.event
@@ -30,9 +36,8 @@ async def on_message(message):
 
 
 def list_issues():
-    api = GhApi()
-    issues = api.issues.list_for_repo(owner="philion", repo="taskbot")
-
+    repo = gh.get_repo("philion/taskbot")
+    issues = repo.get_issues(state='open')
     response_str = "-"
     for issue in issues:
         response_str = f'{response_str}\n{issue}'
@@ -40,21 +45,5 @@ def list_issues():
     return response_str
 
 
-def main(args=None):
-    # load the secrets
-    load_dotenv()
-    discord_token = os.getenv("DISCORD_TOKEN")
-    # github_token = os.getenv("GITHUB_TOKEN") #accessed directly in env
-
-    # run the client
-    client.run(discord_token)
-
-
-if __name__ == '__main__':
-    rc = 1
-    try:
-        main()
-        rc = 0
-    except Exception as e:
-        print('Error: %s' % e, file=sys.stderr)
-    sys.exit(rc)
+# run the client
+client.run(discord_token)
